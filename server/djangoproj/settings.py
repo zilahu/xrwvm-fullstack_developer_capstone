02dc62e51016fe12bd9d97f9b87401adef946d52
Kókai László <kokai.laszlo@gmail.com>
2025-09-07 09:55:52 +0200

    pyhton linter 06

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.'+
+        'django.contrib.auth.password_validation.' +
         'UserAttributeSimilarityValidator',
     },
     {
