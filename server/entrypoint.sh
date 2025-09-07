5f7ea7675363ce7d3ad48405cc26b4f9f67f6be1
Kokai Laszlo <kokai.laszlo@gmail.com>
2025-09-07 05:13:42 -0400

    containerize

---
+++ server/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Make migrations and migrate the database.
+echo "Making migrations and migrating the database. "
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py migrate --noinput --run-syncdb
+python manage.py collectstatic --noinput
+exec "$@"
